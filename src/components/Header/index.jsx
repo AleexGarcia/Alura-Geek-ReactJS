96b24c65cc8215b22a7b2aeaa37b4ec68cb56a92
Alexandre Garcia <alexandre_augusto_garcia@hotmail.com>
2022-09-27 17:41:10 -0300

    input header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,18 +2,34 @@ import styles from './Header.module.scss';
 import logo from '../../assets/Header/Logo.svg';
 import Button from '../Button';
 import classNames from 'classnames';
-
+import React, { useState } from 'react';
 
 export default function Header() {
-
+    const [exibeBusca, setExibeBusca] = useState(false);
+    
     return (
-        <header className={classNames(styles.header,'container') }>
+        <header className={classNames(styles.header, 'container')}>
             <img src={logo} alt='' className={styles.header__logo} />
             <Button color={'secundario'}>Login</Button>
-            <div className={styles.header__inputBox}>
+
+
+            <span
+                onClick={() => {
+                    !exibeBusca ? setExibeBusca(true) : setExibeBusca(false);
+                }}
+                className={classNames(styles.header__lupa, styles.botaoBusca)}
+            >
+
+            </span>
+
+            {exibeBusca && (<div className={styles.header__inputBox}>
                 <input type="search" placeholder='O que deseja encontrar?' />
-                <label className={styles.header__lupa}></label>
-            </div>
+                <label
+                    className={styles.header__lupa}
+                >
+                </label>
+            </div>)}
+            
         </header>
     );
 }
