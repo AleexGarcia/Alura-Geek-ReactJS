0bbf21d98b383758d9cfed26cbd1374e4770c2b1
Alexandre Garcia <alexandre_augusto_garcia@hotmail.com>
2022-09-27 15:40:17 -0300

    colocando as img na pasta public

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,12 @@
 import Produtos from './Produtos';
-import dadosProdutos from '../../dados/produtos.json';
+import dadosProdutos from '../../Data/produtos.json';
 import styles from './Home.module.scss';
-
+import Banner from './Banner';
 export default function Home() {
-        
+
     return (
         <main className={styles.home}>
+            <Banner/>
             <Produtos
                 produtos={dadosProdutos.filter(produto => produto.categoria === 'StarWars')}
             />
