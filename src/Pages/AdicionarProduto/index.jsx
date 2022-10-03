bbb1c81755ae4594ec2c9553bc275140ceccd823
Alexandre Garcia <alexandre_augusto_garcia@hotmail.com>
2022-10-03 11:48:40 -0300

    inputs

--- src/Pages/AdicionarProduto/index.jsx
+++ src/Pages/AdicionarProduto/index.jsx
@@ -4,13 +4,13 @@ import classNames from 'classnames';
 import { useContext, useEffect, useState } from 'react';
 import { HeaderContext } from '../../Context/HeaderContext';
 import { useForm } from 'react-hook-form';
-import { useNavigate, useLocation, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 export default function AdicionarProduto() {
 
     const { register, handleSubmit } = useForm();
     const { setPage } = useContext(HeaderContext);
-    const [propriedade, setPropriedade] = useState({
+    const [values, setValues] = useState({
         titulo: 'Adicionar novo produto',
         botao: 'Adicionar produto',
         imagem: '',
@@ -34,7 +34,7 @@ export default function AdicionarProduto() {
             if (id != undefined) {
                 const resposta = await fetch(`http://localhost:3000/produtos/${id}`);
                 const produto = await resposta.json();
-                setPropriedade({
+                setValues({
                     titulo: 'Editar o produto',
                     botao: 'Editar Produto',
                     imagem: produto.imagem,
@@ -64,30 +64,30 @@ export default function AdicionarProduto() {
     return (
         <main>
             <section className={classNames(styles.adicionar, 'container')}>
-                <h2 className={styles.adicionar__titulo}>{propriedade.titulo}</h2>
+                <h2 className={styles.adicionar__titulo}>{values.titulo}</h2>
                 <form action="" onSubmit={handleSubmit(onSubmit)} className={styles.adicionar__formulario}>
                     <div className={styles.inputBox}>
                         <label htmlFor="">URL da imagem</label>
-                        <input value={propriedade.imagem} required {...register('imagem')} type="text" />
+                        <input value={values.imagem} required {...register('imagem')} type="text" />
                     </div>
                     <div className={styles.inputBox}>
                         <label htmlFor="">Categorias</label>
-                        <input value={propriedade.categoria} required {...register('categoria')} type="text" />
+                        <input value={values.categoria} required {...register('categoria')} type="text" />
                     </div>
                     <div className={styles.inputBox}>
                         <label htmlFor="">Nome do produto</label>
-                        <input value={propriedade.nome} required {...register('nome')} type="text" />
+                        <input value={values.nome} required {...register('nome')} type="text" />
                     </div>
                     <div className={styles.inputBox}>
                         <label htmlFor="">Preço do produto</label>
-                        <input value={propriedade.preco} required {...register('preco')} type="text" />
+                        <input value={values.preco} required {...register('preco')} type="text" />
                     </div>
                     <div className={styles.inputBox}>
-                        <textarea value={propriedade.descricao} required {...register('descricao')} type="text" placeholder='Descrição do produto'>
+                        <textarea value={values.descricao} required {...register('descricao')} type="text" placeholder='Descrição do produto'>
 
                         </textarea>
                     </div>
-                    <Button color={'primario'}>{propriedade.botao}</Button>
+                    <Button color={'primario'}>{values.botao}</Button>
                 </form>
             </section>
         </main>
