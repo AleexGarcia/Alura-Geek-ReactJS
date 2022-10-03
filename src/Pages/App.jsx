eb6fa8775a2e67d4b6c4a8335f9472aa59753929
Alexandre Garcia <alexandre_augusto_garcia@hotmail.com>
2022-10-03 18:44:38 -0300

    rotas privadas

--- src/Pages/App.jsx
+++ src/Pages/App.jsx
@@ -2,15 +2,22 @@
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Home from './Home';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './Login';
 import AdicionarProduto from './AdicionarProduto';
 import Dashboard from './Dashboard';
 import ProdutoPage from './ProdutoPage';
 import Cadastro from './Cadastro';
-import { AuthProvider } from '../Context/AuthContext';
+import { AuthProvider, AuthContext } from '../Context/AuthContext';
 import { HeaderProvider } from '../Context/HeaderContext';
+import { useContext } from 'react';
 
+const PrivateRoute = ({ children, redirectTo }) => {
+    const { authenticated } = useContext(AuthContext);
+
+    return authenticated ? children : <Navigate to={redirectTo} />;
+
+};
 
 function App() {
 
@@ -24,9 +31,21 @@ function App() {
                         <Route path='produto/:id' element={<ProdutoPage />} />
                         <Route path='login' element={<Login />} />
                         <Route path='cadastro' element={<Cadastro />} />
-                        <Route path='dashboard' element={<Dashboard />} />
-                        <Route path='dashboard/adicionar' element={<AdicionarProduto />} />
-                        <Route path='dashboard/editar/:id' element={<AdicionarProduto />} />
+                        <Route path='dashboard' element={
+                            <PrivateRoute redirectTo={'/login'}>
+                                <Dashboard />
+                            </PrivateRoute>
+                        } />
+                        <Route path='dashboard/adicionar' element={
+                            <PrivateRoute redirectTo={'/login'}>
+                                <AdicionarProduto />
+                            </PrivateRoute>
+                        } />
+                        <Route path='dashboard/editar/:id' element={
+                            <PrivateRoute redirectTo={'/login'}>
+                                <AdicionarProduto />
+                            </PrivateRoute>
+                        } />
                     </Routes>
                     <Footer />
                 </Router>
